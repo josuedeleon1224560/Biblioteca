929d00a4a23717cd1b1ac5fec16a2a5ff0e744a1
josuedeleon1224560 <josuedeleon1224560@gmail.com>
2021-01-12 16:36:54 -0600

    Subiendo proyecto de la universidad sobre diseño web basico

---
+++ js.js
@@ -0,0 +1,18 @@
+document.getElementById('titulo').innerHTML = "Biblioteca";
+
+
+function mOver(obj) {
+  obj.innerHTML = "Thank You"
+}
+
+function mOut(obj) {
+  obj.innerHTML = "Mouse Over Me"
+}
+
+function Tiempo{
+	var = t new Date();
+	var = h get.Hours();
+	var = m get.Minutes();
+	var = s get.Seconds();
+document.body.innerHTML= t.h + t.m + t.s;
+}
